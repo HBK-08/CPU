b71d071b58a28c2b03495880dc7cdd6b18a17a05
HBK-08 <208583474@qq.com>
2025-09-13 11:23:36 +0800

    Update RegFile.sv

--- RegFile.sv
+++ RegFile.sv
@@ -1,37 +1,39 @@
 `include "core_struct.vh"
 module RegFile (
-  input clk,
-  input rst,
-  input we,
-  input CorePack::reg_ind_t  read_addr_1,
-  input CorePack::reg_ind_t  read_addr_2,
-  input CorePack::reg_ind_t  write_addr,
-  input  CorePack::data_t write_data,
-  output CorePack::data_t read_data_1,
-  output CorePack::data_t read_data_2
+    input clk,
+    input rst,
+    input we,                               // write enable,get from the decoder
+    input CorePack::reg_ind_t  read_addr_1,
+    input CorePack::reg_ind_t  read_addr_2,
+    input CorePack::reg_ind_t  write_addr,
+    input CorePack::data_t     write_data,
+    output CorePack::data_t    read_data_1,
+    output CorePack::data_t    read_data_2
 );
-  import CorePack::*;
 
-  integer i;
-  data_t register [1:31]; // x1 - x31, x0 keeps zero
+    import CorePack::*;
 
-  // fill your code
-  
-  // initialize the RegFile
-  initial begin
-    for (i = 0; i < 32; i = i + 1)
-      register[i] = 0;
-  end
+    integer i;
+    data_t register[1:31]; // x1 - x31,x0 === 0
 
-  // write
-  always @(posedge clk) begin
-    if (we && write_addr)                               // if write_addr == 0 ,then it can't be wirtten
-      register[write_addr] <= write_data;
-  end
+    integer i;
 
-  // read
-    assign read_data_1 = register[read_addr_1];
-    assign read_data_2 = register[read_addr_2];
+    // fill your code
+    // write
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+            for (i = 1; i < 32; i = i + 1) begin        // initialize the RegFile
+                register[i] <= 64'b0;                   // Non-blocking Assignment
+            end
+        end
+        
+        else if (we && write_addr != 5'd0) begin        // if write_addr == 0 ,then it can't be wirtten
+            register[write_addr] <= write_data;         // Non-blocking Assignment
+        end
+    end
 
+    // read
+    assign read_data_1 = (read_addr_1 == 5'd0) ? 64'b0 : register[read_addr_1]; // read_data_1
+    assign read_data_2 = (read_addr_2 == 5'd0) ? 64'b0 : register[read_addr_2]; // read_data_2
 
 endmodule
